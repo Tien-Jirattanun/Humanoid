091bbf226d16dde5d43535273a40e7d28ae584d4
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2025-06-19 19:58:11 +0700

    clean code & update link
    - change the parent link to torso

--- src/hanuman_control/src/position_control/position_control.cpp
+++ src/hanuman_control/src/position_control/position_control.cpp
@@ -4,19 +4,23 @@ double PositionControl::PIDControl(double setpoint, double current_position) {
     double error = setpoint - current_position;
     double delta_u = 0.0;
 
+    // PID velocity form
     delta_u = kp_ * (error - e_prev_one_) +   
               ki_ * error +                    
               kd_ * (error - 2 * e_prev_one_ + e_prev_two_);  
 
+    // Check if the control output is within the limits
     if (!((u_prev_ >= u_max_ && delta_u > 0) || (u_prev_ <= u_min_ && delta_u < 0))) {
         delta_u = u_prev_ + delta_u;  
     } else {
         delta_u = u_prev_;  
     }
 
+    // clamping 
     delta_u = (delta_u > u_max_) ? u_max_ : delta_u;
     delta_u = (delta_u < u_min_) ? u_min_ : delta_u;
 
+    // update previous values
     e_prev_two_ = e_prev_one_;
     e_prev_one_ = error;
     u_prev_ = delta_u;
